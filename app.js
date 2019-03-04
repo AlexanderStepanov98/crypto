81b6db2c89ab01e3b7090e2501022e8aaa849754
ostepano <ostepano@softserveinc.com>
2019-03-04 13:00:12 +0200

    OTP algorythm logic fixed; Added empty console.logs for correct output

--- app.js
+++ app.js
@@ -11,27 +11,36 @@ let vigenereKey = "QWERTY";
 
 
 console.log("Original text : " + File.getText(testFile));
+console.log("");
 
 
 console.log('// -------------------------- Caesar algorithm test ---------------------------- //');
+console.log("Caesar key = " + caesarKey);
 Caesar.encodeFile(testFile, caesarKey);
 console.log(File.getText(testFile));
 Caesar.decodeFile(testFile, caesarKey);
+console.log("");
 
 
 console.log('// ---------------------- Transposition algorithm test ------------------------- //');
+console.log("Transposition key = " + transposeKey);
 Transpose.encodeFile(testFile, transposeKey);
 console.log(File.getText(testFile));
 Transpose.decodeFile(testFile, transposeKey);
+console.log("");
 
 
 console.log('// ------------------------- Vigenere algorithm test --------------------------- //');
+console.log("Vigenere key = " + vigenereKey);
 Vigenere.encodeFile(testFile, vigenereKey);
 console.log(File.getText(testFile));
 Vigenere.decodeFile(testFile, vigenereKey);
+console.log("");
 
 
 console.log('// ---------------------------- OTP algorithm test ------------------------------ //');
 let otpKey = OTP.encodeFile(testFile);
+console.log("OTP key = " + otpKey);
 console.log(File.getText(testFile));
 OTP.decodeFile(testFile, otpKey);
+console.log("");
--- otp.js
+++ otp.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
-const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-                "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d",
-                "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
-                "y", "z", ":", "1", "2", "3", "4", "5", "6", "7",
-                "8", "9", "0", " "];
+// const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+//                 "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+//                 "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d",
+//                 "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+//                 "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+//                 "y", "z", ":", "1", "2", "3", "4", "5", "6", "7",
+//                 "8", "9", "0", " "];
 
 function ascii(char) {
     return char.charCodeAt(0);
@@ -18,28 +18,16 @@ function generateKey(messageLength) {
     for (var i = 0; i < messageLength; i++) {
         key += String.fromCharCode(Math.floor(Math.random() * (91 - 65)) + 65);
     }
-    console.log("key = " + key);
+
     return key;
 
 }
 
-function encryption(text, key, enable) {
+function encryption(text, key) {
 
-    let index = 0;
     let cipher = '';
     for (let i = 0; i < text.length; i++) {
-        // cipher += String.fromCharCode(ascii(text[i]) ^ ascii(key[i]));
-        if (enable) {
-            cipher += alphabet[(alphabet.indexOf(text[i]) + alphabet.indexOf(key[i])) % 64];
-        } else {
-            index = (alphabet.indexOf(text[i]) - alphabet.indexOf(key[i]));
-            if (index < 0) {
-                cipher += alphabet[alphabet.length + index];
-            } else {
-                cipher += alphabet[index];
-            }
-
-        }
+        cipher += String.fromCharCode(ascii(text[i]) ^ ascii(key[i]));
     }
 
     return cipher;
@@ -51,13 +39,13 @@ function encryption(text, key, enable) {
 exports.encodeFile = function (file) {
     var text = fs.readFileSync(file, 'utf8');
     var key = generateKey(text.length);
-    text = encryption(text, key, true);
+    text = encryption(text, key);
     fs.writeFileSync(file, text);
     return key;
 };
 
 exports.decodeFile = function (file, key) {
     var text = fs.readFileSync(file, 'utf8');
-    text = encryption(text, key, false);
+    text = encryption(text, key);
     fs.writeFileSync(file, text);
 };
